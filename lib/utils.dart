e0596856736727d3020b022d6fa910cc83778196
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-03-20 14:31:43 +0100

    Update code with DayProps and add merge function

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,4 +1,6 @@
 import 'package:excel/excel.dart';
+import 'package:collection/collection.dart';
+import 'package:json_to_notices/model.dart';
 
 const basis = 'Basis';
 
@@ -9,12 +11,17 @@ void downloadExcel() {
   Sheet basisSheet = excel.sheets[basis]!;
   basisSheet.appendRow(createHeaderRow());
 
-  DAYOFTHEWEEK.values.forEach(
-    (day) {
+  int start = 3;
+  DAYOFTHEWEEK.values.forEachIndexed(
+    (index, day) {
       basisSheet.appendRow(createBlankRow());
       createWeekDay(day).forEach((element) {
         basisSheet.appendRow(element);
       });
+
+      merge(basisSheet, start, day, 'A');
+      merge(basisSheet, start, day, 'R');
+      start += dayOfTheWeekMapping[day]!.numberOfRows + 1;
     },
   );
 
@@ -40,20 +47,14 @@ List<List<CellValue?>> createWeekDay(DAYOFTHEWEEK day) {
   List<List<CellValue?>> cells = [];
   List<CellValue?> cellValues = [];
 
-  int startingHour, maxRows;
-  if (day == DAYOFTHEWEEK.Samstag || day == DAYOFTHEWEEK.Sonntag) {
-    startingHour = 10;
-    maxRows = 22;
-  } else {
-    startingHour = 15;
-    maxRows = 12;
-  }
+  int startingHour = dayOfTheWeekMapping[day]!.startingHour;
+  int numberOfRows = dayOfTheWeekMapping[day]!.numberOfRows;
   String fullHourMinutes = ':00';
   String halfHourMinutes = ':30';
   String formatedTime = '';
-  for (int rowIndex = 1; rowIndex <= maxRows; rowIndex++) {
+  for (int rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
     cellValues = [];
-    if (rowIndex % 2 == 0) {
+    if (rowIndex % 2 == 1) {
       formatedTime =
           '$startingHour$halfHourMinutes-${startingHour + 1}$fullHourMinutes';
       startingHour++;
@@ -75,23 +76,51 @@ List<List<CellValue?>> createWeekDay(DAYOFTHEWEEK day) {
 }
 
 enum DAYOFTHEWEEK {
-  Montag,
-  Dienstag,
-  Mittwoch,
-  Donnerstag,
-  Freitag,
-  Samstag,
-  Sonntag
+  montag,
+  dienstag,
+  mittwoch,
+  donnerstag,
+  freitag,
+  samstag,
+  sonntag
 }
 
-Map day_of_the_week_mapping = {
-  DAYOFTHEWEEK.Montag: 'MO',
-  DAYOFTHEWEEK.Dienstag: 'DI',
-  DAYOFTHEWEEK.Mittwoch: 'MI',
-  DAYOFTHEWEEK.Donnerstag: 'DO',
-  DAYOFTHEWEEK.Freitag: 'FR',
-  DAYOFTHEWEEK.Samstag: 'SA',
-  DAYOFTHEWEEK.Sonntag: 'SO'
+Map<DAYOFTHEWEEK, DayProps> dayOfTheWeekMapping = {
+  DAYOFTHEWEEK.montag: const DayProps(
+    startingHour: 15,
+    endingHour: 21,
+    name: 'MO',
+  ),
+  DAYOFTHEWEEK.dienstag: const DayProps(
+    startingHour: 15,
+    endingHour: 21,
+    name: 'DI',
+  ),
+  DAYOFTHEWEEK.mittwoch: const DayProps(
+    startingHour: 15,
+    endingHour: 21,
+    name: 'MI',
+  ),
+  DAYOFTHEWEEK.donnerstag: const DayProps(
+    startingHour: 15,
+    endingHour: 21,
+    name: 'DO',
+  ),
+  DAYOFTHEWEEK.freitag: const DayProps(
+    startingHour: 15,
+    endingHour: 21,
+    name: 'FR',
+  ),
+  DAYOFTHEWEEK.samstag: const DayProps(
+    startingHour: 10,
+    endingHour: 19,
+    name: 'SA',
+  ),
+  DAYOFTHEWEEK.sonntag: const DayProps(
+    startingHour: 10,
+    endingHour: 21,
+    name: 'SO',
+  )
 };
 
 List<CellValue?> createBlankRow() {
@@ -100,4 +129,13 @@ List<CellValue?> createBlankRow() {
     cellValues.add(const TextCellValue(''));
   }
   return cellValues;
+}
+
+void merge(Sheet sheet, int start, DAYOFTHEWEEK day, String colName) {
+  int end = start + dayOfTheWeekMapping[day]!.numberOfRows - 1;
+  sheet.merge(
+    CellIndex.indexByString('$colName$start'),
+    CellIndex.indexByString('$colName$end'),
+    customValue: TextCellValue(dayOfTheWeekMapping[day]!.name),
+  );
 }
